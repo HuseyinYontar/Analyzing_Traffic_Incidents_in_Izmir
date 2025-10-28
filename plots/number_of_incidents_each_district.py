fd1b338fddd25f13d590754cc949a53a3586d918
Hüseyin Yontar <120130524+HuseyinYontar@users.noreply.github.com>
2025-10-28 20:32:52 +0300

    Update on some plots

--- plots/number_of_incidents_each_district.py
+++ plots/number_of_incidents_each_district.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+plt.rcParams.update({
+    "font.family": "serif",
+    "font.serif": ["Times New Roman"],
+    "font.size": 16,
+})
 # Load the dataset
 df = pd.read_excel("../dataCleaning/izbb-kaza-ariza-verileri_with_ilce_updated_with_gun_tipi.xlsx")
 
@@ -15,10 +19,10 @@ plt.figure(figsize=(12, 6))
 ilce_counts.plot(kind="bar")
 
 #plt.title("Number of Incidents by District between\n2021-12-1 - 2025-09-28")
-plt.xlabel("District")
-plt.ylabel("Number of Incident")
+plt.xlabel("District", fontsize=18)
+plt.ylabel("Number of Incident", fontsize=18)
 plt.xticks(rotation=45, ha="right")
 plt.tight_layout()
 
-plt.savefig("incidents_by_district.png", dpi=300, bbox_inches="tight", facecolor="white")
+plt.savefig("incidents_by_district.pdf", dpi=300, bbox_inches="tight", facecolor="white")
 plt.show()
